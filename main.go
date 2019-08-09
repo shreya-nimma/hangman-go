734178168038cd1a771a3db31ebf1c698a2a3ece
S-Nimma <shreyanimma@yahoo.com>
2019-08-10 01:21:36 +0530

    First commit. Basic Go program setup done.

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func main() {
+	fmt.Println(" ")
+	fmt.Println(">> Welcome to Hangman in Go!")
+	fmt.Println(">> Are you ready to play (Y/N) ?")
+}
